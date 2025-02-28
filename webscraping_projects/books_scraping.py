ba2dc270af41f6c98cf499591710fcdeefc673cd
carlosmichelutti <carlos.stz590@gmail.com>
2025-02-27 22:03:33 -0300

    Update books scraping.
    Update(crawler) - Update books scraping.

--- webscraping_projects/books_scraping.py
+++ webscraping_projects/books_scraping.py
@@ -47,7 +47,7 @@ class BooksScraping:
     session: requests.Session = requests.Session()
     session.headers.update(
         {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
         }
     )
 
